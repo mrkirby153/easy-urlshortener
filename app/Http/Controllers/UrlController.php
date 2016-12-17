234f283de21d02729865610b4b44e174830364c8
mrkirby153 <mr.austinwhyte@gmail.com>
2016-12-17 10:45:31 -0800

    Implement more efficient way of deleting clicks

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -6,74 +6,78 @@ use App\Click;
 use App\ShortenedUrl;
 use Illuminate\Http\Request;
 
-class UrlController extends Controller
-{
+class UrlController extends Controller {
 
 
-    public function index(){
+    public function index() {
         return view('main');
     }
 
 
-    public function create(Request $request){
+    public function create(Request $request) {
         $url = $request->url;
         $custom = $request->custom_alias;
 
         $this->validate($request, [
-            'url'=>'required|URL',
-            'custom_alias'=>'unique:shortened_urls,id|alpha_num'
+            'url' => 'required|URL',
+            'custom_alias' => 'unique:shortened_urls,id|alpha_num'
         ], [
-            'url.required'=>'Please specify a URL',
-            'url.u_r_l'=>'Please enter a valid URL',
-            'custom_alias.unique'=>'This alias is already taken',
-            'custom_alias.alpha_num'=>'Aliases can only have the characters A-Z and 0-9'
+            'url.required' => 'Please specify a URL',
+            'url.u_r_l' => 'Please enter a valid URL',
+            'custom_alias.unique' => 'This alias is already taken',
+            'custom_alias.alpha_num' => 'Aliases can only have the characters A-Z and 0-9'
         ]);
 
         $shortenedUrl = new ShortenedUrl();
-        if($custom != ''){
+        if ($custom != '') {
             $shortenedUrl->id = $custom;
         } else {
             $id = $this->generateId(5);
-            if($id == "E:MAX_TRIES_EXCEEDED"){
-                return response()->json(['url'=>'Could not generate a shortened URL. If this issue persists, we have run out of IDs'], 422);
+            if ($id == "E:MAX_TRIES_EXCEEDED") {
+                return response()->json(['url' => 'Could not generate a shortened URL. If this issue persists, we have run out of IDs'], 422);
             }
             $shortenedUrl->id = $id;
         }
         $shortenedUrl->long_url = $url;
         $shortenedUrl->owner = (\Auth::guest()) ? -1 : \Auth::id();
         $shortenedUrl->save();
-        return response()->json(['success'=>$shortenedUrl->id]);
+        return response()->json(['success' => $shortenedUrl->id]);
     }
 
-    public function all(Request $request){
-        if(\Auth::guest())
+    public function all(Request $request) {
+        if (\Auth::guest())
             return response()->json([]);
         $raw_urls = ShortenedUrl::whereOwner(\Auth::id())->orderBy('created_at', 'desc')->get();
         $urls = array();
-        foreach($raw_urls as $u){
-            $u->short_url = $request->root().'/'.$u->id;
+        foreach ($raw_urls as $u) {
+            $u->short_url = $request->root() . '/' . $u->id;
             $u->click_count = $u->clicks()->count();
             $urls[] = $u;
         }
         return $urls;
     }
 
-    public function delete(Request $request){
+    public function delete(Request $request) {
         $url = ShortenedUrl::whereId($request->url)->first();
-        if($url == null){
-            return response()->json(['error'=>'That URL does not exist'], 422);
+        if ($url == null) {
+            return response()->json(['error' => 'That URL does not exist'], 422);
         }
-        if($url->owner != \Auth::id()){
-            return response()->json(['error'=>'You do not have permission to delete this URL'], 422);
+        if ($url->owner != \Auth::id()) {
+            return response()->json(['error' => 'You do not have permission to delete this URL'], 422);
         }
-        foreach($url->clicks as $click){
-            $click->delete();
+        if ($url->clicks()->count() < 1500)
+            foreach ($url->clicks as $click) {
+                $click->delete();
+            }
+        else {
+            \Log::info("Deleting where id = $url->id");
+            \DB::table('clicks')->where('url', '=', $url->id)->delete();
         }
         $url->delete();
-        return response()->json(['success'=>'URL deleted!']);
+        return response()->json(['success' => 'URL deleted!']);
     }
 
-    public function click($id){
+    public function click($id) {
         $url = ShortenedUrl::findOrFail($id);
         $click = new Click();
         $click->id = $this->generateId(30);
@@ -83,7 +87,7 @@ class UrlController extends Controller
         return response()->redirectTo($url->long_url);
     }
 
-    private function generateId($size){
+    private function generateId($size) {
         $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         $url = "";
         $tries = 0;
@@ -93,7 +97,7 @@ class UrlController extends Controller
             }
             $tries += 1;
         } while (ShortenedUrl::whereId($url)->first() != null && $tries < 1000);
-        if($tries >= 1000){
+        if ($tries >= 1000) {
             return "E:MAX_TRIES_EXCEEDED";
         }
         return $url;
