fcd791eabcd348d8ac5e69e43d7ed419f9ffff8b
mrkirby153 <mr.austinwhyte@gmail.com>
2016-12-19 21:07:52 -0800

    Fall back to the URL if an exception occurres when retrieving the title

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -137,13 +137,16 @@ class UrlController extends Controller {
     }
 
     private function get_title($url){
-        $str = file_get_contents($url);
-        if(strlen($str)>0){
-            $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
-            preg_match("/\\<title\\>(.*)\\<\\/title\\>/i",$str,$title); // ignore case
-            if(sizeof($title) < 1)
-                return $url;
-            return $title[1];
+        try{
+            $str = file_get_contents($url);
+            if(strlen($str)>0){
+                $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
+                preg_match("/\\<title\\>(.*)\\<\\/title\\>/i",$str,$title); // ignore case
+                return $title[1];
+            }
+        } catch (\Exception $e){
+            return $url;
         }
+
     }
 }
